98fe9b046516a68689d01fbdf6d70b735a3dd4ea
Dhiraj Khandare <dhirajkhandare76@gmail.com>
2026-05-11 22:16:41 +0530

    Add files via upload

---
+++ Advance Java/01 JDBC/01 Introduction Code for JDBC/src/demo/Demo.java
@@ -0,0 +1,26 @@
+package demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/myjdbc","root","pass1234");
+			
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
